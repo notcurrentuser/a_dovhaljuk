d8eaaab2bd73e07d0c4c914f33ed5bbc476959aa
notcurrentuser <notcurrentuser@gmail.com>
2023-05-14 17:10:02 +0300

    [frontend] - get_pem/pem

--- frontend/pages/get_pem.py
+++ frontend/pages/get_pem.py
@@ -4,22 +4,24 @@ import requests
 
 def auth_key(page, redirect):
     password_field = ft.TextField(label="Your password")
+    key_field = ft.TextField(label="Your key")
 
     def check_password(e):
         if password_field.value:
-            page.splash = ft.ProgressBar()
-            page.update()
             try:
-                pems = requests.post('http://localhost:5465/private_key/generation/',
-                                     {'password_phrase': password_field.value}).json()
-                page.splash = None
-                page.update()
+                if key_field.value:
+                    page.session.set('encrypted_private_key', key_field.value)
+                else:
+                    page.splash = ft.ProgressBar()
+                    page.update()
+                    pems = requests.post('http://localhost:5465/private_key/generation/',
+                                         {'password_phrase': password_field.value}).json()
+                    page.splash = None
+                    page.update()
 
-                encrypted_private_key = pems['encrypted_pem_private_key']
-                public_key = pems['pem_public_key']
+                    encrypted_private_key = pems['encrypted_pem_private_key']
 
-                page.session.set('encrypted_private_key', encrypted_private_key)
-                page.session.set('public_key', public_key)
+                    page.session.set('encrypted_private_key', encrypted_private_key)
 
                 redirect(page, None)
             except requests.exceptions.ConnectionError:
@@ -31,4 +33,4 @@ def auth_key(page, redirect):
             password_field.error_text = "Please enter your password"
             page.update()
 
-    page.add(password_field, ft.ElevatedButton("Generation keys", on_click=check_password))
+    page.add(password_field, key_field, ft.ElevatedButton("Generation keys", on_click=check_password))
--- frontend/pages/pem.py
+++ frontend/pages/pem.py
@@ -3,7 +3,4 @@ import flet as ft
 
 
 def pem(page):
-    page.add(ft.Text('encrypted_private_key'))
-    page.add(ft.Text(page.session.get('encrypted_private_key')))
-    page.add(ft.Text("pem_public_key"))
-    page.add(ft.Text(page.session.get('public_key')))
+    page.add(ft.TextField(label='Private Key', read_only=True, value=page.session.get('encrypted_private_key')))
